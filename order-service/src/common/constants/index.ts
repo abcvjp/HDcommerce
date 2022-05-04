e44f76c2cad23c25f2f32bd7d782ebddba48e379
Ha Van Hoai <hoai.ha@ekoios.vn>
2022-05-04 21:54:42 +0700

    chore: fix sort query param, default sort

--- order-service/src/common/constants/index.ts
+++ order-service/src/common/constants/index.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_DBQUERY_LIMIT = 30;
-export const DEFAULT_DBQUERY_SORT: Record<string, 1 | -1> = { _id: 1 };
+export const DEFAULT_DBQUERY_SORT: Record<string, 1 | -1> = { createdAt: -1 };
 
 export enum UserRole {
   USER = 1,
