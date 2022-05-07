47ee5dbba714025463a93221226a20aa05d15286
Ha Van Hoai <hoai.ha@ekoios.vn>
2022-05-07 21:10:41 +0700

    chore(user): remove junk code

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -9,12 +9,11 @@ import appConfig from './config/app.config';
 import dbConfig from './config/db.config';
 
 import { DatabaseModule } from './database';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ExceptionInterceptor } from './common/interceptors/exception.interceptor';
 import { ResponseSerializator } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { RPCExceptionFilter } from './common/filters/rpc-exception.filter';
-import { SortQueryParamPipe } from './common/pipes/sort-query-param.pipe';
 import brokerConfig from './config/broker.config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -38,10 +37,6 @@ import { HttpLoggerMiddleware } from './common/middlewares/http-logger.middlewar
       provide: APP_GUARD,
       useClass: RolesGuards,
     },
-    // {
-    // provide: APP_PIPE,
-    // useClass: SortQueryParamPipe,
-    // },
     {
       provide: APP_INTERCEPTOR,
       useClass: ExceptionInterceptor,
