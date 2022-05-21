0d91595dbc0b7ce439826f809bab259ffef3d373
Ha Van Hoai <hoai.ha@ekoios.vn>
2022-05-21 23:04:02 +0700

    feat: setup production env

---
+++ konga.user.js
@@ -0,0 +1,12 @@
+module.exports = [
+	{
+			"username": "hoaideptrai",
+			"email": "hoaideptrai@example.com",
+			"firstName": "Ha",
+			"lastName": "Hoai",
+			"node_id": "http://kong:8001",
+			"admin": true,
+			"active" : true,
+			"password": "123456"
+	},
+]
